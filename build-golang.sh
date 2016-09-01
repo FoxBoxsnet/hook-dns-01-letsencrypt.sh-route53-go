5235487fbcc475b244360d6a7fe32485100cf8b9
foxboxsnet <naokiaoyama2009@gmail.com>
2016-09-02 00:06:33 +0900

    fix

--- build-golang.sh
+++ build-golang.sh
@@ -9,6 +9,10 @@ echo .
 go get github.com/${GIT_USERNAME}/${GIT_PROJECT_REPONAME}
 go get github.com/mitchellh/gox
 go get github.com/tcnksm/ghr
+
+git clone https://github.com/${GIT_USERNAME}/${GIT_PROJECT_REPONAME}
+cd ${GIT_PROJECT_REPONAME}
+
 gox -ldflags "-s" -output "dist/route53_{{.OS}}_{{.Arch}}"
 ghr -t ${GITHUB_TOKEN} -u ${GIT_USERNAME} -r ${GIT_PROJECT_REPONAME} `git tag` dist/
 
